0f45783bd7db553a99ccc947aef653af7551d18c
kaiesmahmud <kaiesmahmud@gmail.com>
2023-04-30 11:14:12 +0600

    projects data added

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,10 +3,10 @@ import dummmy from './../assets/nehal-website-redesign.jpg'
 import Link from "next/link"
 
 const Card = ({project}) =>(
-      <div className="bg-white/80 rounded shadow overflow-hidden">
-        <Image src={project.img || dummmy} alt={project.name} width={400} height={200}/>
+      <div className="bg-white/80 rounded shadow overflow-hidden pt-5 bg-slate-100">
+        <Image src={project.imagesUrl || dummmy} alt={project.name} width={700} height={500}/>
         <div className="p-2 md:p-4 lg:p-5 flex flex-col gap-2">
-          <p className="font-bold text-sm md:text-base lg:text-lg">{project.name}</p>
+          <p className="font-semibold text-sm md:text-base lg:text-lg">{project.name}</p>
           <div className="flex text-xs flex-wrap gap-1">
             {
               project.techs.map(i => (<div key={i} className="bg-slate-600 text-white p-1 rounded px-2 text-xs">{i}</div>))
@@ -35,7 +35,7 @@ const Projects = () => {
       live: 'https://kaiesmahmud.github.io/My-first-portfolio/',
       github:'https://github.com/kaiesmahmud/My-first-portfolio',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/first-website.jpg'
     },
     {
       name:'Hot Gadget E-com Practice',
@@ -43,7 +43,7 @@ const Projects = () => {
       live: 'https://kaiesmahmud.github.io/demo-e-commerce-2/#product',
       github:'https://github.com/kaiesmahmud/demo-e-commerce-2',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/e-com-2.jpg'
     },
     {
       name:'First Static E-commerce Website',
@@ -51,7 +51,7 @@ const Projects = () => {
       live: 'https://kaiesmahmud.github.io/first-e-commerce/',
       github:'https://github.com/kaiesmahmud/first-e-commerce',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:['']
+      imagesUrl:'/projects/e-com.jpg'
     },
     {
       name:'HTML-Css Custom Loader',
@@ -59,7 +59,7 @@ const Projects = () => {
       live:'https://kaiesmahmud.github.io/loading-animation-effects/',
       github:'https://github.com/kaiesmahmud/loading-animation-effects',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/loader.jpg'
     },
     {
       name:'Auto Typing Animation HTML-CSS',
@@ -67,7 +67,23 @@ const Projects = () => {
       live:'https://kaiesmahmud.github.io/my-short-intro-with-typing-animation/',
       github:'https://github.com/kaiesmahmud/my-short-intro-with-typing-animation',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/auto-typing.jpg'
+    },
+    {
+      name:'Survey Form Design | FCC | HTML-CSS',
+      description:"I learn and create my first static html,css, bootstrap website in 2021",
+      live:'https://codepen.io/KaiesMahmud/full/wveNyQV?editors=1100',
+      github:'/',
+      techs:['HTML', 'Css', 'BootStrap 5','FCC'],
+      imagesUrl:'/projects/survey.jpg'
+    },
+    {
+      name:'Documentation page | FCC | HTML-CSS',
+      description:"I learn and create my first static html,css, bootstrap website in 2021",
+      live:'https://codepen.io/KaiesMahmud/full/jOLEPPE',
+      github:'/',
+      techs:['HTML', 'Css', 'BootStrap 5','FCC'],
+      imagesUrl:'/projects/documentation.jpg'
     },
   ]
   const jsProjects = [
@@ -77,7 +93,7 @@ const Projects = () => {
       live: 'https://kaiesmahmud.github.io/calculator-using-simple-js/',
       github:'https://github.com/kaiesmahmud/calculator-using-simple-js',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/simple-calculator.jpg'
     },
     {
       name:'Clock ',
@@ -85,7 +101,7 @@ const Projects = () => {
       live: 'https://kaiesmahmud.github.io/Clock/',
       github:'https://github.com/kaiesmahmud/Clock',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/clock.jpg'
     },
     {
       name:'Github Finder',
@@ -93,7 +109,7 @@ const Projects = () => {
       live: 'https://kaiesmahmud.github.io/Github-Finder/',
       github:'https://github.com/kaiesmahmud/Github-Finder',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:['']
+      imagesUrl:'/projects/github-finder.jpg'
     },
     {
       name:'Tic-Tac-Toe Game',
@@ -101,7 +117,7 @@ const Projects = () => {
       live:'https://kaiesmahmud.github.io/tic-tac-toe-game-js/',
       github:'https://github.com/kaiesmahmud/tic-tac-toe-game-js',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/tic-tac-game.jpg'
     },
     {
       name:'Drone ',
@@ -109,27 +125,96 @@ const Projects = () => {
       live:'https://kaiesmahmud.github.io/Drone/',
       github:'https://github.com/kaiesmahmud/Drone',
       techs:['HTML', 'Css', 'BootStrap 5',],
-      imagesUrl:''
+      imagesUrl:'/projects/drone.jpg'
+    },
+  ]
+  const reactProjects = [
+    {
+      name:'DevdeerTech | Nextjs',
+      description: "I learn and create my first static html,css, bootstrap website in 2021",
+      live: 'https://devdeer.tech/',
+      github:'/',
+      techs:['HTML', 'Css', 'React','Nextjs','TailwindCss', 'Swiper.js', 'Grid','Flex','Mobile Responsive','Chat Widget','Github','React-icons','Framer motion',],
+      imagesUrl:'/projects/devdeer-home.jpg'
+    },
+    {
+      name:'Events App | Nextjs ',
+      description:"I learn and create my first static html,css, bootstrap website in 2021",
+      live:'https://n-events-app-nextjs.vercel.app/',
+      github:'https://github.com/kaiesmahmud/Events-App-Nextjs',
+      techs:['HTML', 'Css','JavaScript','Nextjs','React','TailwindCss', 'Material-UI','RapidAPI',"API Integration",'Grid','Flex','Mobile Responsive','Github','React-icons','Filter-Search'],
+      imagesUrl:'/projects/events.jpg'
+    },
+
+    {
+      name:'Fitness | Vitejs | React',
+      description: "I learn and create my first static html,css, bootstrap website in 2021",
+      live: 'https://fitness-app-vitejs.vercel.app/exercise/0002',
+      github:'https://github.com/kaiesmahmud/Fitness-app-vitejs',
+      techs:['HTML', 'Css', 'JavaScript','React', 'Vitejs','TailwindCss', 'Material-UI','RapidAPI',"API Integration",'Grid','Flex','Github','React-icons','Pagination','Fetch Related topic'],
+      imagesUrl:'/projects/fitness.jpg'
+    },
+    {
+      name:'Restaurent App | React | Vitejs',
+      description:"I learn and create my first static html,css, bootstrap website in 2021",
+      live:'https://res2rent-vitejs-nehal.vercel.app/',
+      github:'https://github.com/kaiesmahmud/Restaurent-Vitejs',
+      techs:['HTML', 'Css', 'JavaScript','Vitejs','React','TailwindCss', 'Material-UI','RapidAPI',"API Integration",'Grid','Flex','Mobile Responsive','Github','React-icons'],
+      imagesUrl:'/projects/restaurent.jpg'
+    },
+    {
+      name:'RealState App | Nextjs ',
+      description:"I learn and create my first static html,css, bootstrap website in 2021",
+      live:'https://realstate-nextjs-nehal.vercel.app/',
+      github:'https://github.com/kaiesmahmud/Realstate-Nextjs',
+      techs:['HTML', 'Css','JavaScript','Nextjs','React','TailwindCss', 'Material-UI','RapidAPI',"API Integration",'Grid','Flex','Mobile Responsive','Github','React-icons','Filter-Search'],
+      imagesUrl:'/projects/realstate.jpg'
+    },
+    {
+      name:'News App | Nextjs ',
+      description:"I learn and create my first static html,css, bootstrap website in 2021",
+      live:'https://news-app-nextjs-nehal.vercel.app/',
+      github:'https://github.com/kaiesmahmud/NewsApp-react-nextjs',
+      techs:['HTML', 'Css', 'JavaScript','Nextjs','React','TailwindCss','NewsAPI',"API Integration",'Grid','Flex','Mobile Responsive','Github','React-icons','Lodash','Pagination','Framer-motion'],
+      imagesUrl: "https://i.ibb.co/R0QD2TK/news-app.jpg" ,
+
+    },
+    {
+      name:'E-Green | E-commerce | Vitejs ',
+      description: "I learn and create my first static html,css, bootstrap e-commerce website in 2021",
+      live: 'https://egreen-vitejs-ecommerce.vercel.app/',
+      github:'https://github.com/kaiesmahmud/E-Green-vitejs-ecommerce',
+      techs:['HTML', 'Css', 'React','Vitejs', 'TailwindCss', "API",'Swiper.js', 'Grid','Flex','Mobile Responsive','Github','React-icons'],
+      imagesUrl:'/projects/e-green.jpg'
+    },
+
+    {
+      name:'Portfolio App | vitejs ',
+      description:"I learn and create my first static html,css, bootstrap website in 2021",
+      live:'https://sohel-seo-specialist.netlify.app/',
+      github:'https://github.com/kaiesmahmud/sohel-portfolio',
+      techs:['HTML', 'Css','JavaScript','Vitejs','React','TailwindCss',,'Grid','Flex','Mobile Responsive','Github','React-icons'],
+      imagesUrl:'/projects/portfolio.jpg'
     },
   ]
   return (
     <section id="projects" className=" min-h-[90vh] pt-[11vh]">
       <h1 className="text-lg md:text-2xl lg:text-3xl xl:text-4xl text-center font-semibold">My Projects</h1>
       {/* -------------------REACT PROJECTS  ---------------------------- */}
-      <div className="px-5 md:px-10">
+      <div className="px-5 md:px-10 pt-10">
         <h2 className="my-5 p-1 font-semibold text-lg md:text-xl lg:text-2xl border-l-8 border-sky-500">REACT :</h2>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-5 lg:gap-10 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 gap-y-10 md:gap-5 lg:gap-10 ">
           {
-            htmlProjects.map((project,index)=>
+            reactProjects.map((project,index)=>
             <Card project={project} key={index} />
             )
           }
         </div>
       </div>
       {/* -------------------jAVASCRIPT PROJECTS  ---------------------------- */}
-      <div className="px-5 md:px-10">
+      <div className="px-5 md:px-10 pt-10">
         <h2 className="my-5 p-1 font-semibold text-lg md:text-xl lg:text-2xl border-l-8 border-yellow-400"> JAVASCRIPT :</h2>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-5 lg:gap-10 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-5 lg:gap-10 ">
           {
             jsProjects.map((project,index)=>
               <Card project={project} key={index} />
@@ -139,9 +224,9 @@ const Projects = () => {
       </div>
 
       {/* -------------------HTML-CSS PROJECTS  ---------------------------- */}
-      <div className="px-5 md:px-10">
-        <h2 className="my-5 p-1 font-semibold text-lg md:text-xl lg:text-2xl border-l-8 border-red-500">HTML :</h2>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-5 lg:gap-10 ">
+      <div className="px-5 md:px-10 pt-10">
+        <h2 className="my-5 p-1 font-semibold text-lg md:text-xl lg:text-2xl border-l-8 border-red-500">HTML-CSS :</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-5 lg:gap-10 ">
           {
             htmlProjects.map((project,index)=>
             <Card project={project} key={index} />
